bd735e320927cbf9f0d19665b5f762db30b5b00c
Hoàng Tuấn Anh <81757605+tuananhhoang1805@users.noreply.github.com>
2022-09-23 15:08:14 +0700

    add image icon

---
+++ asset/image.js
@@ -0,0 +1,22 @@
+import earthImage from "./image/earth.png";
+import accountImage from "./image/icons8-account-100.png";
+import notiImage from "./image/icons8-notification-100.png";
+import homeImage from "./image/icons8-house-100.png";
+import eventImage from "./image/icons8-calendar-100.png";
+import settingImage from "./image/icons8-settings-100.png";
+import messImage from "./image/icons8-mail-100.png";
+import videoImage from './image/icons8-video-call-100.png'
+import cameraImage from './image/icons8-camera-100.png'
+import emoji from './image/icons8-anime-emoji-100.png'
+export {
+  earthImage,
+  accountImage,
+  notiImage,
+  homeImage,
+  eventImage,
+  settingImage,
+  messImage,
+  videoImage,
+  cameraImage,
+  emoji
+};
