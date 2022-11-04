4357396dc836b97ca39e84699e972105e25755a7
Hoàng Tuấn Anh <81757605+tuananhhoang1805@users.noreply.github.com>
2022-11-04 08:58:40 +0700

    fix ui

--- components/ImageDetail.js
+++ components/ImageDetail.js
@@ -3,10 +3,9 @@ import React from "react";
 
 export const ImageDetail = (props) => {
   const { images } = props;
-  console.log(props.images.url);
   return (
     <div className="flex flex-wrap gap-x-4">
-      {props.images.map((image) => {
+      {images.map((image) => {
         return (
           <div className="h-[200px] w-[200px]">
             <img
